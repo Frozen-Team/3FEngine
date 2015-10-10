a41b64a816df78c71f2f724d0831c82b150cca5a
OTER <maxymhammer@yandex.ru>
2015-10-10 23:53:51 +0300

    Add scripts for cleaning and generating solution

--- 3dparty/clean.bat
+++ 3dparty/clean.bat
@@ -1,5 +1,6 @@
 @RD /S /Q solution
-@RD /S /Q install
+@RD /S /Q install\x64
+@RD /S /Q install\x86
 del "build_d.log"
 del "build.log"
 del "install_d.log"
---
+++ clean.bat
@@ -0,0 +1,2 @@
+@RD /S /Q solution
+del "generate.log"
