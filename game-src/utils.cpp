3c608e7e43c9f5f459f0ce16ecd85a199593889b
OTER <maxymhammer@yandex.ru>
2015-10-12 01:14:14 +0300

    Add utils

---
+++ game-src/utils.cpp
@@ -0,0 +1,13 @@
+#include "utils.h"
+#include <algorithm>
+
+namespace FEngine {
+	namespace Utils {
+		std::string ToLower(const std::string & str)
+		{
+			auto lower_case = str;
+			std::transform(lower_case.begin(), lower_case.end(), lower_case.begin(), ::tolower);
+			return lower_case;
+		}
+	}
+}
---
+++ game-src/utils.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+namespace FEngine {
+	namespace Utils {
+		std::string ToLower(const std::string & str);
+	}
+}
