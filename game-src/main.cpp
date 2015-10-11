5a21481c1f84b005ecdf3823ab4138194ae83270
OTER <maxymhammer@yandex.ru>
2015-10-12 01:14:38 +0300

    Add some test in main file. To delete

--- game-src/main.cpp
+++ game-src/main.cpp
@@ -4,6 +4,13 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <assert.h>
+
+#include "settings.h"
+
+#include "Eigen\Dense"
+
+
 
 
 const int SCREEN_WIDTH = 640;
@@ -11,6 +18,17 @@ const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[])
 {
+	FEngine::Settings s;
+	s.LoadDefaultSettings();
+	std::cout << s.Get<int>(FEngine::Settings::SettingsNames::WindowHeight) << std::endl;
+	std::cout << s.Get<int>(FEngine::Settings::SettingsNames::WindowWidth) << std::endl;
+
+	s.Set<int>("param1", 5006600);
+	std::cout << s.Get<int>("param1");	
+	s.Set<float>("param1", 1.2f);
+	std::cout << s.Get<float>("param1");
+
+	system("pause");
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -20,8 +38,10 @@ int main(int argc, char* args[])
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-	auto window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+	auto window = SDL_CreateWindow("Main window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
 	if (window == NULL)
 	{
 		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -37,12 +57,21 @@ int main(int argc, char* args[])
 	} else {
 		std::cout << "Beda" << std::endl;
 	}
+	SDL_GL_SetSwapInterval(-1);
 	
-	
-
-	SDL_Delay(2000);
-
-
+	auto i = 0;
+	while (true)
+	{
+		i++;
+		if (i > 100) {
+			break;
+		}
+		SDL_Delay(1);
+		glClearColor(float(rand() % 256) / 256, float(rand() % 256) / 256, float(rand() % 256) / 256, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
+		//SDL_GL_
+	}
 
 	SDL_DestroyWindow(window);
 
@@ -52,3 +81,7 @@ int main(int argc, char* args[])
 
 	return 0;
 }
+
+//int main(int argc, char* args[]) {
+//	return 0;
+//}
