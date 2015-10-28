eb09e4a1d8e728561b487f8ecf92dcb761d486c7
OTER <maxymhammer@yandex.ru>
2015-10-29 00:49:59 +0200

    Integrate GLEW

--- TODO.md
+++ TODO.md
@@ -3,6 +3,7 @@
 
 ###CMake:
 
+- **Make patch files for 3dparty libraries**  
 - *Print version info for each library.*  
 - *Test\get it done for OSX, Linux.*  
 - Remove FBXSDK from 3FEngine dependency. Only fbx2fseq use it
--- src/renderer/gl/f_gl_helper.hpp
+++ src/renderer/gl/f_gl_helper.hpp
@@ -1,6 +1,7 @@
-#ifndef _3FENGINE_SRC_RENDERER_GL_HPP_
-#define _3FENGINE_SRC_RENDERER_GL_HPP_
+#ifndef _3FENGINE_SRC_RENDERER_GL_F_GL_HELPER_HPP_
+#define _3FENGINE_SRC_RENDERER_GL_F_GL_HELPER_HPP_
 
+#include "GL\glew.h"
 #include "SDL.h"
 #include "SDL_opengl.h"
 
@@ -8,21 +9,31 @@
 #include "utils/f_singleton.hpp"
 
 namespace fengine {
-
+	// Helper must be constructed right after context creation.
 	class GlHelper : public futils::FSingleton<GlHelper>
 	{
 	public:
-		GlHelper();
 		typedef enum
 		{
 			kGlExtFramebufferBlit,
-
 		} GlExtension;
+	public:
+		GlHelper();
+		
+		static bool IsExtensionSupported(const FString& ext_name);
+
+		static bool IsExtensionSupported(GlExtension ext);
 
 	private:
-		const static FMap<GlExtension, FString> extension_names_;
-		static FMap<FString, 
+		static const FMap<GlExtension, FString> ext_names_;
+		static FVector<FString> exts_supported_;
+		static FString gl_vendor_;
+		static FString gl_renderer_;
+		static FString gl_ver_;
+		static FString gl_shading_lang_ver_;
+		int major_;
+		int minor_;
 	};
 }
 
-#endif // _3FENGINE_SRC_RENDERER_GL_HPP_
+#endif // _3FENGINE_SRC_RENDERER_GL_F_GL_HELPER_HPP_
